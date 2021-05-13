451aa9e2fda289a62ea8eada9fe5d1da64832976
katurkmen <kaanturkmen@protonmail.com>
2021-05-13 12:51:51 +0300

    Initial commit.

---
+++ RPGCode.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- University: Koç University
+-- Students: Kaan Türkmen - Can Usluel
+-- 
+-- Create Date:    12:45:10 05/13/2021 
+-- Design Name: FPGA Design
+-- Module Name:    RPGCode - Behavioral 
+-- Project Name: RandomPasswordGenerator
+-- Target Devices: Prometheus FPGA
+-- Tool versions: 
+-- Description: Creates a random password sequence according to the user inputs.
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity RPGCode is
+end RPGCode;
+
+architecture Behavioral of RPGCode is
+
+begin
+
+
+end Behavioral;
+
